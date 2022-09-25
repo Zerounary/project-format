db4eb668d1d92d40c3b603559581c18c09e5ac8d
宋诚 <song.c@sungeon.com>
2022-09-25 10:07:36 +0800

    添加目录遍历示例

--- examples/templatedir.rs
+++ examples/templatedir.rs
@@ -1,7 +1,26 @@
-use handlebars::Handlebars;
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
 
+// one possible implementation of walking a directory only visiting files
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}
 
 fn main() {
-  let h = Handlebars::new();
-
+    visit_dirs(Path::new("./src"), &|dir| {
+        println!("{:?}",dir.path());
+        println!("{:?}",dir.path().extension());
+    });
 }
