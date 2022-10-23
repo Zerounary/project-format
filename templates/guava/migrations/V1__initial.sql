4b8d96440c2b95ad4c9f324c917cd2069f7048c4
Zerounary <song.c@sungeon.com>
2022-10-23 22:25:17 +0800

    feat: 自动补充 ddl 的系统字段

--- templates/guava/migrations/V1__initial.sql
+++ templates/guava/migrations/V1__initial.sql
@@ -1,3 +1,10 @@
-{{#each table_ddls }}
+{{#each tables }}
+{{#each ddls }}
 {{this}}
+{{/each}}
+alter table {{name}} ADD tenant_id int8;
+alter table {{name}} ADD create_time datetime;
+alter table {{name}} ADD create_user_id int8;
+alter table {{name}} ADD modify_time datetime;
+alter table {{name}} ADD modify_user_id int8;
 {{/each}}
