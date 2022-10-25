96a6ff6a909a7c5fd4af9a2e1c2627c099944ef4
Zerounary <song.c@sungeon.com>
2022-10-25 14:06:53 +0800

    feat: 添加 可用 字段

--- templates/guava/migrations/V1__initial.sql
+++ templates/guava/migrations/V1__initial.sql
@@ -7,4 +7,5 @@ alter table {{name}} ADD create_time datetime;
 alter table {{name}} ADD create_user_id int8;
 alter table {{name}} ADD modify_time datetime;
 alter table {{name}} ADD modify_user_id int8;
+alter table {{name}} ADD is_active bool default true;
 {{/each}}
