5944f2a798de9355092ff8aaf8fba2e09c3501d4
Zerounary <song.c@sungeon.com>
2023-02-27 10:39:06 +0800

    feat: task sql 中增加；

--- templates/melon/src/main/resources/db/cmp/tasks/{tables;name}.sql
+++ templates/melon/src/main/resources/db/cmp/tasks/{tables;name}.sql
@@ -2,5 +2,5 @@
 insert into SYS_SCHEDULE_CRON
 (id, name, cron, created_time, updated_time)
 select get_sequences('SYS_SCHEDULE_CRON'), '{{name}}', '{{cron}}', sysdate, sysdate
-from dual
+from dual;
 {{/if}}
