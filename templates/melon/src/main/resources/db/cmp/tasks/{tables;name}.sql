1b42c3068accbdce5eb76abafb746c6f51b92765
Zerounary <song.c@sungeon.com>
2023-03-01 14:12:21 +0800

    feat: 添加组织新增接口

--- templates/melon/src/main/resources/db/cmp/tasks/{tables;name}.sql
+++ templates/melon/src/main/resources/db/cmp/tasks/{tables;name}.sql
@@ -1,4 +1,5 @@
 {{#if cron }}
+--changeset data:a
 insert into SYS_SCHEDULE_CRON
 (id, name, cron, created_time, updated_time)
 select get_sequences('SYS_SCHEDULE_CRON'), '{{name}}', '{{cron}}', sysdate, sysdate
